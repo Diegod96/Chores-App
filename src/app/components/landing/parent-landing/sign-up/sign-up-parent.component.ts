93cac97683445ee5937ed8cfe84bc7bbdb57c7cd
taylorpalm <taylor.palm@yahoo.com>
2019-10-06 18:50:33 -0400

    auth

--- src/app/components/landing/parent-landing/sign-up/sign-up-parent.component.ts
+++ src/app/components/landing/parent-landing/sign-up/sign-up-parent.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {Router} from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-sign-up-parent',
@@ -9,7 +10,8 @@ import {Router} from '@angular/router';
 })
 export class SignUpParentComponent implements OnInit {
 
-  constructor(private router: Router) {
+  constructor(private router: Router,
+              private authService: AuthService) {
   }
 
   signUp = new FormGroup({
@@ -33,7 +35,7 @@ export class SignUpParentComponent implements OnInit {
 
     const email = form.value.email;
     const password = form.value.password;
-    //this.authService.loginUser(email, password);
+    this.authService.signUpParent(email, password);
 
     console.log(email, password);
     this.router.navigate(['chores']);
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,20 +1,68 @@
 import { Injectable } from '@angular/core';
+import * as firebase from 'firebase';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  token;
   user;
+  uid;
 
-  constructor() {
+
+  constructor(private router: Router) {
 
     this.user = 'parent';
   }
 
 
+  signUpParent(email: string, password: string){
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then(
+      response =>{
+        firebase.auth().currentUser.getIdToken().then(
+          (token: string) => {
+              this.token = token;
+                this.uid = firebase.auth().currentUser.uid;
+                console.log(this.uid)
+          }).catch(
+          error => {console.log(error);
+          });
+        });
+      }
+    
 
+
+
+  loginParent(email: string, password: string) {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(
+            response => {
+
+                // this.router.navigate(['/accounts']);
+                firebase.auth().currentUser.getIdToken().then(
+                    (token: string) => {
+                        this.token = token;
+                        this.uid = firebase.auth().currentUser.uid;
+                        console.log(this.uid)
+                    }).catch(
+                    error => {console.log(error);
+                    });
+            });
 }
 
 
-//Hi 
+logout() {
+    firebase.auth().signOut().then(() => console.log('logged you out')).catch(
+        error => console.log(error)
+    );
+    this.token = null;
+    // this.router.navigate(['']);
+}
+
+
+
+}
+
