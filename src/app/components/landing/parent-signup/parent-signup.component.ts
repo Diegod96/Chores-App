3ebf0eb7009fc53a7b279049dc640cac5b86c0e7
Diegod96 <ddphillyfan@gmail.com>
2019-11-13 07:57:33 -0500

    Morning work

--- src/app/components/landing/parent-signup/parent-signup.component.ts
+++ src/app/components/landing/parent-signup/parent-signup.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {FormControl, FormGroup, Validators, FormBuilder} from '@angular/forms';
 import {Router} from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 import { ParentService } from 'src/app/services/parent.service';
+import {AbstractControl} from '@angular/forms';
+import { PasswordValidation } from './password-validation';
 
 @Component({
   selector: 'app-parent-signup',
@@ -13,13 +15,16 @@ export class ParentSignupComponent implements OnInit {
 
 
   constructor(private router: Router,
-    private authService: AuthService,
-    private parentService: ParentService) {
+              private authService: AuthService,
+              private parentService: ParentService) {
 }
 
 signUp = new FormGroup({
     email : new FormControl('', [Validators.required, Validators.email]),
-    password : new FormControl('', [Validators.required])
+    password : new FormControl('', [Validators.required]),
+    confirmation : new FormControl('', [Validators.required])
+}, {
+    validators: PasswordValidation.MatchPassword
 });
 
 
@@ -31,17 +36,42 @@ get password() {
 return this.signUp.get('password');
 }
 
+get confirmation() {
+  return this.signUp.get('confirmation');
+
+}
+
+
+
 
 onSignUp(form) {
 
   const email = form.value.email;
   const password = form.value.password;
+  const confirmation = form.value.confirmation;
   this.authService.signUpParent(email, password);
   console.log(email, password);
   this.router.navigate(['home']);
 }
 
-ngOnInit() {
+// MatchPassword(AC: AbstractControl) {
+//   const password = AC.get('password').value; // Get value in password field
+//   const confirmPassword = AC.get('confirmPassword').value; // Get value in confirm password field
+//   // tslint:disable-next-line: triple-equals
+//   if (password != confirmPassword) {
+//       console.log('Passwords do not match!!!');
+//       AC.get('confirmPassword').setErrors( {MatchPassword: true} );
+//     } else {
+//       console.log('Passwords match');
+//       return null;
+//     }
+// }
+
+
+ngOnInit(): void {
 }
 
 }
+
+
+
