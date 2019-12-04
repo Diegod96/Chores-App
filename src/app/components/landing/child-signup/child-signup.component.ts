e50a697e84b67a4fd302590a85a4472a85326c4d
Caseys4 <Caseys4@go.stockton.edu>
2019-12-03 22:06:34 -0500

    Child Signup

--- src/app/components/landing/child-signup/child-signup.component.ts
+++ src/app/components/landing/child-signup/child-signup.component.ts
@@ -1,69 +1,62 @@
-import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {Router} from '@angular/router';
-import { AuthService } from 'src/app/services/auth.service';
-import { ChildService } from 'src/app/services/child.service';
-
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { ChildService } from 'src/app/services/child.service';
 @Component({
-  selector: 'app-child-signup',
-  templateUrl: './child-signup.component.html',
-  styleUrls: ['./child-signup.component.css']
+  selector: 'app-child-signup',
+  templateUrl: './child-signup.component.html',
+  styleUrls: ['./child-signup.component.css']
 })
-export class ChildSignupComponent implements OnInit {
-
-  constructor(private router: Router,
-    private authService: AuthService,
-    private childService: ChildService) {
+export class ChildSignupComponent implements OnInit {
+  constructor(private router: Router,
+    private authService: AuthService,
+    private childService: ChildService) {
 }
 
-signUp = new FormGroup({
-    parentID : new FormControl('', [Validators.required]),
-    email : new FormControl('', [Validators.required, Validators.email]),
-    name: new FormControl('', [Validators.required]),
-    password : new FormControl('', [Validators.required])
+signUp = new FormGroup({
+  parentID : new FormControl('', [Validators.required]),
+  email : new FormControl('', [Validators.required, Validators.email]),
+  name: new FormControl('', [Validators.required]),
+  password : new FormControl('', [Validators.required, Validators.minLength(8)]),
+  confirmation : new FormControl('', [Validators.required, Validators.minLength(8)])
 });
 
-get parentID() {
-  return this.signUp.get('parentID');
-  }
-
-get email() {
-return this.signUp.get('email');
+get parentID() {
+  return this.signUp.get('parentID');
+  }
+get email() {
+return this.signUp.get('email');
+}
+get name() {
+  return this.signUp.get('name');
+  }
+get password() {
+return this.signUp.get('password');
+}
+get confirmation() {
+return this.signUp.get('confirmation');
 }
 
-get name() {
-  return this.signUp.get('name');
-  }
+onSignUp(form) {
+  const email = form.value.email;
+  const password = form.value.password;
+  const confirmation = form.value.confirmation;
+  const parentID = form.value.parentID;
+  const name = form.value.name;
+  const promise = Promise.resolve(this.authService.signUpChild(email, password, parentID, name));
 
-get password() {
-return this.signUp.get('password');
+  promise.then(res => {
+    this.router.navigate(['home']);
+  }).catch(err => {
+    alert (err.message);
+  });
+  // this.authService.signUpChild(email, password, parentID, name);
+  
+  // console.log(email, password, parentID, name);
+  // this.router.navigate(['child-chores']);
 }
-
-
-onSignUp(form) {
-  const email = form.value.email;
-  const password = form.value.password;
-  const confirmation = form.value.confirmation;
-  const parentID = form.value.parentID;
-  const name = form.value.name;
-  const promise = Promise.resolve(this.authService.signUpChild(email, password, parentID, name));
-
-
-  promise.then(res => {
-    this.router.navigate(['home']);
-  }).catch(err => {
-    alert (err.message);
-  });
-  // this.authService.signUpChild(email, password, parentID, name);
-
-  
-
-  // console.log(email, password, parentID, name);
-  // this.router.navigate(['child-chores']);
+ngOnInit() {
 }
-
-ngOnInit() {
-}
-
 
 }
